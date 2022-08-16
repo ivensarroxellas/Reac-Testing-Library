a1729ad380056ea7ab394a5d89bf03bba7c60a8d
Ivens <ivensarroxellas@hotmail.com>
2022-08-16 17:52:29 -0300

    Req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('NotFound', () => {
+  test('Teste mensagem No favorite, caso a pessoa não tenha pokémons favoritos',
+    () => {
+      renderWithRouter(<NotFound />);
+      const checkNotFound = screen.getByRole('heading', {
+        name: /Page requested not found/i, level: 2 });
+
+      expect(checkNotFound).toBeInTheDocument();
+    });
+
+  test('Teste se a página mostra a imagem',
+    () => {
+      renderWithRouter(<NotFound />);
+      const checkImage = screen.getByAltText(/Pikachu crying/i);
+
+      expect(checkImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
